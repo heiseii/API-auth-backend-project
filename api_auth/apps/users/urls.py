33c6fe1dd32520011656861173b7c168fad6b78b
lucca <lukaweinzettel@gmail.com>
2026-03-09 22:42:33 -0300

    .

--- api_auth/apps/users/urls.py
+++ api_auth/apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView, ProfileView, RoleAssignView, RoleListView
+from .views import RegisterView, ProfileView, RoleAssignView, RoleListView, PermissionListView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("me/", ProfileView.as_view(), name="profile"),
     path("roles/", RoleListView.as_view(), name="roles"),
+    path('permissions/', PermissionListView.as_view(), name='permissions')
 ]
